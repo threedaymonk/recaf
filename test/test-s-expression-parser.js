0c7eb0a88378386b739798316d6208533cf9bd69
Paul Battley <pbattley@gmail.com>
2011-05-25 23:50:40 +0100

    Alias the identifier construct to make things more readable.

--- test/test-s-expression-parser.js
+++ test/test-s-expression-parser.js
@@ -1,6 +1,10 @@
 var common = require('./common');
 var parser = require('sexp-parser.js');
 
+var $i = function(v){
+  return ['identifier', v];
+}
+
 var should_parse = function(name, input, expected){
   exports[name] = function(test){
     test.deepEqual(parser.parse(input), expected);
@@ -10,32 +14,32 @@ var should_parse = function(name, input, expected){
 
 should_parse('variable declarations',
   'var cubes, list, math, num, number, opposite, race, square;',
-  ['script',
-    ['var',
-      ['identifier', 'cubes'],
-      ['identifier', 'list'],
-      ['identifier', 'math'],
-      ['identifier', 'num'],
-      ['identifier', 'number'],
-      ['identifier', 'opposite'],
-      ['identifier', 'race'],
-      ['identifier', 'square']]]
+  [ 'script',
+    [ 'var',
+      $i('cubes'),
+      $i('list'),
+      $i('math'),
+      $i('num'),
+      $i('number'),
+      $i('opposite'),
+      $i('race'),
+      $i('square') ]]
 );
 
 should_parse('dot calls',
   'var __slice = Array.prototype.slice;',
-  ['script',
-    ['var',
-      [ ['identifier', '__slice'],
-        ['dot',
-          ['dot',
-            ['identifier', 'Array'],
-            ['identifier', 'prototype']],
-          ['identifier', 'slice']]]]]
+  [ 'script',
+    [ 'var',
+      [ $i('__slice'),
+        [ 'dot',
+          [ 'dot',
+            $i('Array'),
+            $i('prototype')],
+          $i('slice') ]]]]
 );
 
 should_parse('assignment',
   'number = 42;',
-  ['script',
-    ['assign', ['identifier', 'number'], 42]]
+  [ 'script',
+    [ 'assign', $i('number'), 42 ]]
 );
