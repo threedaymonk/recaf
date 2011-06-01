4cfdf1efadca8640133e82aff34d688c0fa4c4f1
Paul Battley <pbattley@gmail.com>
2011-06-01 17:18:55 +0100

    Generate variable definitions and objects.

--- test/test-coffeescript-generator.js
+++ test/test-coffeescript-generator.js
@@ -14,11 +14,37 @@ var should_generate = function(name, input, expected){
 };
 
 should_generate('number',
-  [ 'number', 42 ],
+  $n(42),
   '42'
 );
 
 should_generate('string',
-  [ 'string', "'Hello!' said Bob." ],
+  $s("'Hello!' said Bob."),
   "'\\\'Hello!\\\' said Bob.'"
+);
+
+should_generate('single variable declaration',
+  [ 'var', $i('foo') ],
+  'foo = null'
+);
+
+should_generate('multiple variable declaration',
+  [ 'var', $i('foo'), $i('bar') ],
+  'foo = null\nbar = null'
+);
+
+should_generate('variable initialization',
+  [ 'var', [ $i('foo'), $n(1) ]],
+  'foo = 1'
+);
+
+should_generate('object',
+  [ 'var',
+    [ $i('a'),
+      [ 'object_init',
+        [ $i('b'), $s('one') ],
+        [ $i('c'), $n(2) ]]]],
+  'a =\n'+
+  '  b : \'one\'\n'+
+  '  c : 2'
 );
